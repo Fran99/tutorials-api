18e800e7a013e384fcfeaf1d6b9f433e3173db53
Fran <franciscopm1982@gmail.com>
2023-02-13 18:35:15 -0300

    Update usersRouter.js

--- api/src/components/users/usersRouter.js
+++ api/src/components/users/usersRouter.js
@@ -1,8 +1,41 @@
 const express = require('express');
+const { User } = require('../../../models').sequelize.models;
 
 const usersRouter = express.Router();
 
-usersRouter.get('');
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const user = (await User.create({
+      ...req.body,
+    }, User.attributes())).toJSON();
+    const {
+      password, createdAt, updatedAt, ...userToSend
+    } = user;
+    res.status(201).json({
+      code: 201,
+      data: userToSend,
+    });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+usersRouter.get('/:id', async (req, res) => {
+  try {
+    const {
+      id, name, lastname, email,
+    } = await db.models.User.findByPk(req.params.id);
+
+    res.status(200).json({
+      code: 200,
+      data: {
+        id, name, lastname, email,
+      },
+    });
+  } catch (e) {
+    res.status(404).end();
+  }
+});
 
 module.exports = {
   usersRouter,
