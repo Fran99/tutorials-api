2864346dceac6a308cd47ef65aa824d40fc90ed5
Fran <franciscopm1982@gmail.com>
2023-02-13 18:35:04 -0300

    Update authRouter.js

--- api/src/components/auth/authRouter.js
+++ api/src/components/auth/authRouter.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const { createToken } = require('../../jwt');
-// const db = require('../../models/index');
+const { User } = require('../../../models').sequelize.models;
+const { compare } = require('bcrypt');
+const { AuthenticationError } = require('../../errors/AuthenticationError');
+const { authentication } = require('../../middlewares/authentication');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res, next) => {
+  const { email, password } = req.body;
+  // TODO validate this data
+
+  const user = await User.findOne({
+    where: { email },
+  });
+
+  if (!user || !await compare(password, user.password)) {
+    return next(new AuthenticationError());
+  }
+
+  const token = await createToken({
+    id: user.id,
+    email,
+    name: user.name,
+    lastname: user.lastname,
+  });
 
   res.json({
     code: 200,
     data: {
-      token: 'Token',
+      token,
     },
+  });
+});
+
+router.get('/me', authentication, (req, res) => {
+  res.json({
+    code: 200,
+    data: req.me,
   });
 });
 
