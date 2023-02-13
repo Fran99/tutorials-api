bee2aad1caf5f2cf13bc1c1d418f89daeac6bbd0
Fran <franciscopm1982@gmail.com>
2023-02-13 18:32:08 -0300

    Create middlewares

--- api/src/middlewares/authentication.js
+++ api/src/middlewares/authentication.js
@@ -1,4 +1,17 @@
-module.exports.authentication = (req, res, next) => {
-  console.log('Auth middleware');
+const { AuthenticationError } = require('../errors/AuthenticationError');
+const jwt = require('../jwt');
+const { User } = require('../../models').sequelize.models;
+
+module.exports.authentication = async (req, res, next) => {
+  try {
+    const bearer = req.headers.authorization?.split(' ')[1];
+    const token = await jwt.verifyToken(bearer);
+    if (!token) return next(new AuthenticationError());
+    const { iat, exp, ...me } = token;
+    req.me = me;
+  } catch (e) {
+    next(new AuthenticationError(e));
+  }
+  // req.user = await User.find;
   return next();
 };
--- api/src/middlewares/validation.js
+++ api/src/middlewares/validation.js
@@ -1,1 +1,11 @@
-const joi = require('joi');
+/**
+ * Validates a component schema against some input data, usually req.body
+ * @param {{validate: Function}} schema
+ * @returns {(function(*, *, *): void)|*}
+ */
+module.exports.validation = (schema) => (req, res, next) => {
+  const { error, data } = schema.validate(req.body);
+  console.log(data);
+  if (error) return next(error);
+  return next();
+};
