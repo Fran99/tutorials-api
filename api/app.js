53fad4427c9d4f2225fff209946fbf732522559d
Fran <franciscopm1982@gmail.com>
2023-02-13 18:32:34 -0300

    Update configurations

--- api/app.js
+++ api/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const logger = require('morgan');
-
 const { usersRouter } = require('./src/components/users/usersRouter');
 const { tutorialsRouter } = require('./src/components/tutorials/tutorialsRouter');
 const { authRouter } = require('./src/components/auth/authRouter');
 const { authentication } = require('./src/middlewares/authentication');
 const { authorization } = require('./src/middlewares/authorization');
+const { customErrors } = require('./src/middlewares/errors');
+const db = require('./models/index');
 
 const app = express();
+
+(async () => {
+  await db.sequelize.run();
+})();
 
 app.use([
   logger('dev'),
@@ -23,5 +28,6 @@ app.use([
 
 app.use('/users', usersRouter);
 app.use('/tutorials', tutorialsRouter);
+app.use(customErrors);
 
 module.exports = app;
