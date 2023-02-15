718d0c765408079f52b11d05fd15ac9764351ea8
Fran <franciscopm1982@gmail.com>
2023-02-14 21:57:30 -0300

    Check token against db for better security

--- api/src/middlewares/authentication.js
+++ api/src/middlewares/authentication.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('../errors/AuthenticationError');
 const jwt = require('../jwt');
+const { User } = require('../../models').sequelize.models;
 
 /**
  * Authentication middleware
@@ -8,13 +9,17 @@ const jwt = require('../jwt');
  * @param {Function} next
  * @returns {Promise<*>}
  */
-module.exports.authentication = async (req, res, next) => {
+module.exports.authenticationMiddleware = async (req, res, next) => {
   try {
     const bearer = req.headers.authorization?.split(' ')[1];
     const token = await jwt.verifyToken(bearer);
     if (!token) return next(new AuthenticationError());
-    const { iat, exp, ...me } = token;
-    req.me = me;
+
+    // Let's grab the user from the DB
+    const user = await User.findByPk(token.id);
+    if (!user) return next(new AuthenticationError());
+
+    req.me = user.parse();
     return next();
   } catch (e) {
     return next(new AuthenticationError(e));
